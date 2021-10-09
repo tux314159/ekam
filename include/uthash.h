45978f51adbf0356c301fb62f902117a9a03f69a
Isaac <tux314159@gmail.com>
2021-10-09 16:57:34 +0800

    Updated copyright notices

--- include/uthash.h
+++ include/uthash.h
@@ -2,7 +2,7 @@
 Copyright (c) 2003-2021, Troy D. Hanson     https://troydhanson.github.io/uthash/
 All rights reserved.
 
-Modified slightly by Tux :)
+Modified slightly by Isaac "Tux" Yeo :)
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
