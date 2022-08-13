494de5ce15aa70ced5e423a17892a654b828637b
Isaac <tux314159@gmail.com>
2022-08-13 20:00:12 +0800

    gperf stuff

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 echo "Temporary build script - will be replaced once we can build ourselves."
 cflags=$(cat compile_flags.txt | tr '\n' ' ')
-gcc $cflags -o main $@ src/graph.c src/safealloc.c src/main.c && echo "Built."
+gcc -pg $cflags -o main $@ src/graph.c src/safealloc.c src/main.c && echo "Built."
