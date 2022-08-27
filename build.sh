0b13fd50f3e142d87e5263bf176a5ba2ff091832
Isaac <tux314159@gmail.com>
2022-08-27 12:40:44 +0800

    Added some fs stuffs but still not really used

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cflags=$(cat compile_flags.txt | tr '\n' ' ')
 [ -z "$CC" ] && CC=gcc
 
 echo "Building main"
-$CC $cflags -o main main.c src/*.c -lpthread -lrt
+$CC $cflags -o main main.c src/*.c -lpthread -lrt -lmd -lgdbm -lgdbm_compat
---
+++ include/fs.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDE_FS
+#include <search.h>
+#include <sys/queue.h>
+#include <ndbm.h>
+
+struct filehash {
+   char *filename;
+   char hash[41];
+   LIST_ENTRY(filehash) entries;
+};
+LIST_HEAD(listhead, filehash);
+
+void
+file_compute_sha1(const char *filename, char buf[41]);
+void
+walkdir_storehash(const char *dirname, struct listhead *list);
+void
+fhlist_to_db(struct listhead *list, DBM *db);
+char **
+get_changed(struct listhead *cur, DBM *old);
+
+#endif
