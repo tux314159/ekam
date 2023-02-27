8e0cd707886f10588a6ef14c2378145e5f5a16be
Isaac <tux314159@gmail.com>
2023-02-27 08:13:05 +0800

    Add unquoted versions of macros

--- include/ekam.h
+++ include/ekam.h
@@ -17,51 +17,53 @@
 	Hashtable_size_t *_main_ht        = ht_create_size_t(ADJLIST_MAP_SZ); \
 	size_t            _main_cnt       = 1;
 
-// GRAPH CONVENIENCE MACROS
-
-// Resolve a target name.
-#define R(x) *ht_get_size_t(_main_ht, Q(x))
-
-// We require all targets to be forward-declared.
-#define DECLARE(target) ht_insert_size_t(_main_ht, Q(target), _main_cnt++);
-
-// Define a target.
-#define _(filename, cmd, ...)                                 \
-	do {                                                      \
-		graph_add_target(                                     \
-			&_main_graph,                                     \
-			R(filename),                                      \
-			(size_t[]){__VA_ARGS__},                          \
-			sizeof((size_t[]){__VA_ARGS__}) / sizeof(size_t), \
-			Q(cmd),                                           \
-			Q(filename)                                       \
-		);                                                    \
-	} while (0)
-
-// Ugly; define a target with no dependencies.
-#define __(filename, cmd) \
-	do {                  \
-		graph_add_target( \
-			&_main_graph, \
-			R(filename),  \
-			NULL,         \
-			0,            \
-			Q(cmd),       \
-			Q(filename)   \
-		);                \
-	} while (0)
-
-#define BUILD_TARGET(targ)                                              \
-	do {                                                                \
-		graph_buildpartial(&_main_graph, &_main_partgraph, R(targ));    \
-		graph_execute(&_main_partgraph, argc == 1 ? 1 : atoi(argv[1])); \
-	} while (0)
-
 #define FREE_EKAM()                     \
 	do {                                \
 		graph_delete(&_main_partgraph); \
 		graph_delete(&_main_graph);     \
 		ht_destroy_size_t(_main_ht);    \
 	} while (0)
+
+// GRAPH CONVENIENCE MACROS
+
+// Most macros here that deal with target names will have two
+// versions: quoted and unquoted. Quoted versions simply quote
+// all applicable arguments and pass them to the unquoted version.
+
+// (R)esolve a target name.
+#define R_(x) *ht_get_size_t(_main_ht, x)
+#define R(x)  R_(Q(X))
+
+// We require all targets to be forward-declared.
+#define DECLARE_(target) ht_insert_size_t(_main_ht, target, _main_cnt++);
+#define DECLARE(target)  DECLARE_(Q(target))
+
+// (D)ependency declaration
+#define D_(filename, cmd, ...)                                \
+	do {                                                      \
+		graph_add_target(                                     \
+			&_main_graph,                                     \
+			R_(filename),                                     \
+			(size_t[]){__VA_ARGS__},                          \
+			sizeof((size_t[]){__VA_ARGS__}) / sizeof(size_t), \
+			cmd,                                              \
+			filename                                          \
+		);                                                    \
+	} while (0)
+#define D(filename, cmd, ...) D_(Q(filename), Q(cmd), __VA_ARGS__)
+
+// (D)ependency declaration but no actual deps
+#define D0_(filename, cmd)                                                    \
+	do {                                                                      \
+		graph_add_target(&_main_graph, R_(filename), NULL, 0, cmd, filename); \
+	} while (0)
+#define D0(filename, cmd) D0_(Q(filename), Q(cmd))
+
+#define BUILD_TARGET_(targ)                                              \
+	do {                                                                \
+		graph_buildpartial(&_main_graph, &_main_partgraph, R_(targ));    \
+		graph_execute(&_main_partgraph, argc == 1 ? 1 : atoi(argv[1])); \
+	} while (0)
+#define BUILD_TARGET(targ) BUILD_TARGET_(Q(targ))
 
 #endif
--- main.c
+++ main.c
@@ -23,41 +23,41 @@ main(int argc, char **argv)
 	DECLARE(test/10)
 
 	// clang-format off
-	_(test/10,
+	D(test/10,
 		echo at 10; sleep 1.0; touch test/10,
 		R(test/2), R(test/3)
 	);
-	_(test/1,
+	D(test/1,
 		echo at 1; sleep 1.0; touch test/1,
 		R(test/3), R(test/4)
 	);
-	_(test/2,
+	D(test/2,
 		echo at 2; sleep 1.0; touch test/2,
 		R(test/5)
 	);
-	__(test/3,
+	D0(test/3,
 		echo at 3; sleep 1.0; touch test/3
 	);
-	_(test/4,
+	D(test/4,
 		echo at 4; sleep 1.0; touch test/4,
 		R(test/5)
 	);
-	__(test/5,
+	D0(test/5,
 		echo at 5; sleep 1.0; touch test/5
 	);
-	_(test/6,
+	D(test/6,
 		echo at 6; sleep 1.0; touch test/6,
 		R(test/2), R(test/8), R(test/9)
 	);
-	_(test/7,
+	D(test/7,
 		echo at 7; sleep 1.0; touch test/7,
 		R(test/1), R(test/10), R(test/6)
 	);
-	_(test/8,
+	D(test/8,
 		echo at 8; sleep 1.0; touch test/8,
 		R(test/3)
 	);
-	_(test/9,
+	D(test/9,
 		echo at 9; sleep 1.0; touch test/9,
 		R(test/3)
 	);
